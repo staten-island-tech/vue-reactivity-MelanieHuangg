2a05bc0f7950a49865311476914697f19f4ad3e7
MelanieHuangg <melanie.huang0306@gmail.com>
2024-02-05 10:14:05 -0500

    vue

---
+++ vite-project/src/components/icons/DestCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>{{ Destination.name }}</h2>
+        <img :src="Destination.img" alt="">
+        <h3>{{ Clicked }}</h3>
+        <button @click="increment">Click Me</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const props =defineProps({
+    // age:Number,
+    // name:String,
+    Destination:Object,
+});
+
+//clicker logic 
+const clicked = ref(0); 
+function increment(){
+    clicked.value++; //will increase the number 0 by increments of 1 
+}
+</script>
+
+<style scoped> 
+/* scoped means no other object will be effected */
+
+img{
+    width:20px;
+    height:300px;
+    object-fit:cover;
+}
+
+</style>
